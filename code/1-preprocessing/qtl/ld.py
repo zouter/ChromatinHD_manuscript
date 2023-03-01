628526362a252280164aa00e976df58c320111cb
Wouter Saelens <wouter.saelens@gmail.com>
2023-03-01 18:17:11 +0100

    add ld script

---
+++ code/1-preprocessing/qtl/ld.py
@@ -0,0 +1,39 @@
+# ---
+# jupyter:
+#   jupytext:
+#     formats: ipynb,py:percent
+#     text_representation:
+#       extension: .py
+#       format_name: percent
+#       format_version: '1.3'
+#       jupytext_version: 1.14.1
+#   kernelspec:
+#     display_name: Python 3 (ipykernel)
+#     language: python
+#     name: python3
+# ---
+
+# %%
+# %load_ext autoreload
+# %autoreload 2
+
+import numpy as np
+import pandas as pd
+
+import matplotlib.pyplot as plt
+import matplotlib as mpl
+
+import seaborn as sns
+sns.set_style('ticks')
+
+import pickle
+
+import tqdm.auto as tqdm
+
+import pathlib
+
+import polars as pl
+
+# %%
+
+# %%

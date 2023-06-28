9209658d2ffda9dab4e50d3d74ee4eab4a4d46eb
Victor F <victor.fernandezdelima@epfl.ch>
2023-06-28 09:59:19 +0200

    generalized function

--- code/9-synthetic_data/vertical_lines.py
+++ code/9-synthetic_data/vertical_lines.py
@@ -3,52 +3,12 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 
-# NF range of x values
-x_min = 0
-x_max = 1
-
-# range of y values
-y_min = 0
-y_max = 1
+# range for x and y values for NF
+x_min, x_max, y_min, y_max = 0, 1, 0, 1
 
 # biological range of x values
-p_min = -10000
-p_max = 10000
+p_min, p_max = -10000, 10000
 
-# select arbitrary positions
-p1 = 0
-p2 = 2000
-p3 = -2000
-
-# convert to NF range
-x1 = (p1 - p_min) / (p_max - p_min) * (x_max - x_min) + x_min
-x2 = (p2 - p_min) / (p_max - p_min) * (x_max - x_min) + x_min
-x3 = (p3 - p_min) / (p_max - p_min) * (x_max - x_min) + x_min
-
-# %%
-# uniform distribution
-y1_distribution = np.linspace(x_min, x_max, 1000)
-# not uniform distribution
-y2_distribution = np.linspace(x_min, x_max, 1000) ** 2
-# not uniform distribution in opposite direction
-y3_distribution = np.linspace(x_min, x_max, 1000) ** 0.5
-
-# %%
-# transform y1_distribution to 2D arry
-d1 = np.column_stack((np.full_like(y1_distribution, x1), y1_distribution))
-d2 = np.column_stack((np.full_like(y2_distribution, x2), y2_distribution))
-d3 = np.column_stack((np.full_like(y3_distribution, x3), y3_distribution))
-
-# add noise to x
-d1[:, 0] = d1[:, 0] + np.random.normal(0, 0.01, d1.shape[0])
-d2[:, 0] = d2[:, 0] + np.random.normal(0, 0.01, d2.shape[0])
-d3[:, 0] = d3[:, 0] + np.random.normal(0, 0.01, d3.shape[0])
-
-# %%
-data = np.concatenate((d1, d2, d3), axis=0)
-dataframe = pd.DataFrame(data, columns=['x', 'y'])
-
-# %%
 def plot_cutsites(df, gene, n_fragments):
     fig, ax = plt.subplots(figsize=(15, 15))
 
@@ -60,5 +20,45 @@ def plot_cutsites(df, gene, n_fragments):
     ax.set_ylim([0, 1])
     ax.set_facecolor('white')
 
+def generate_data(p_min, p_max, x_min, x_max, y_min, y_max, *args):
+    data = []
+
+    for arg in args:
+        n_values, shift_density, noise, shift_slope, p = arg
+
+        # convert to NF range
+        x = (p - p_min) / (p_max - p_min) * (x_max - x_min) + x_min
+
+        # create y values
+        y_distribution = np.linspace(y_min, y_max, n_values) ** shift_density
+
+        # create x values
+        x_distribution = np.full_like(y_distribution, x)
+
+        # add noise to x
+        x_distribution = x_distribution + np.random.normal(0, noise, n_values)
+
+        # add shift to x
+        x_distribution = x_distribution + np.linspace(x_min, x_max, n_values) * shift_slope
+
+        # column bind x and y values
+        data.append(np.column_stack((x_distribution, y_distribution)))
+
+    return data
+
+# parameters for generating data
+args_list = [
+    [1000, 0.5, 0.01, 0.001, 0], # n_values, shift_density, noise, shift_slope, p
+    [500, 0.7, 0.02, 0.002, 4000],
+    [2000, 0.3, 0.015, 0.003, -5000],
+]
+
+data_list = generate_data(p_min, p_max, x_min, x_max, y_min, y_max, *args_list)
+
+data = np.concatenate(data_list, axis=0)
+dataframe = pd.DataFrame(data, columns=['x', 'y'])
+# x = genomic position
+# y = latent time
+# use this to create fragments dir
+
 plot_cutsites(dataframe, 'gene', len(dataframe))
-# %%

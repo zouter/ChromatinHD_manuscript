447bef43c2072aeb84f03bf88c69c4e4380d967c
Wouter Saelens <wouter.saelens@gmail.com>
2023-06-29 15:43:40 +0200

    update structural chips

--- code/1-preprocessing/encode/download_structural_chips.py
+++ code/1-preprocessing/encode/download_structural_chips.py
@@ -1,6 +1,6 @@
 # %%
-%load_ext autoreload
-%autoreload 2
+# %load_ext autoreload
+# %autoreload 2
 
 import numpy as np
 import pandas as pd
@@ -9,8 +9,9 @@ import matplotlib.pyplot as plt
 import matplotlib as mpl
 
 import seaborn as sns
-sns.set_style('ticks')
-%config InlineBackend.figure_format='retina'
+
+sns.set_style("ticks")
+# %config InlineBackend.figure_format='retina'
 
 import pickle
 
@@ -29,11 +30,10 @@ import requests
 
 # %%
 bed_folder = chd.get_output() / "bed" / "gm1282_structural_chipseq"
-bed_folder.mkdir(exist_ok = True, parents = True)
+bed_folder.mkdir(exist_ok=True, parents=True)
 
 # %%
-url = "https://www.encodeproject.org/metadata/?type=Experiment&replicates.library.biosample.donor.organism.scientific_name=Homo+sapiens&assay_title=TF+ChIP-seq&status=released&biosample_ontology.term_name=GM12878&target.label!=EP300&target.label!=POLR2A&assembly=GRCh38&control_type!=*&target.label!=POLR2AphosphoS2&target.label!=POLR2AphosphoS5"
-# url = "https://www.encodeproject.org/search/?type=Experiment&replicates.library.biosample.donor.organism.scientific_name=Homo+sapiens&assay_title=TF+ChIP-seq&status=released&biosample_ontology.term_name=GM12878&target.label!=CTCF&target.label!=EP300&target.label!=POLR2A&assembly=GRCh38&control_type!=*&target.label!=POLR2AphosphoS2&target.label!=POLR2AphosphoS5"
+url = "https://www.encodeproject.org/metadata/?type=Experiment&replicates.library.biosample.donor.organism.scientific_name=Homo+sapiens&assay_title=TF+ChIP-seq&status=released&biosample_ontology.term_name=GM12878&target.label!=EP300&assembly=GRCh38&control_type!=*&target.label!=POLR2AphosphoS2&target.label!=POLR2AphosphoS5"
 
 # %%
 obj = requests.get(url)
@@ -50,25 +50,45 @@ files = files.set_index("file_accession")
 # %%
 files.head()
 files["file_format"].value_counts()
-# files["output_type"].value_counts()
+files["output_type"].value_counts()
 
 # %%
-files = files.loc[files["file_format"] == "bed narrowPeak"]
-# files = files.loc[files["file_format"] == "bed narrowPeak"]
-# files = files.loc[files["output_type"] == "optimal IDR thresholded peaks"]
+files = files.loc[files["file_format"] == "bigWig"]
+files = files.loc[files["output_type"] == "fold change over control"]
 files = files.loc[files["file_assembly"] == "GRCh38"]
 files = files.groupby("experiment_target").first().reset_index()
 files["target"] = files["experiment_target"].str.split("-").str[0]
 
-files = files.loc[files["target"].isin(["YY1", "ZNF143", "RAD21", "CTCF"])]
+files = files.loc[
+    files["target"].isin(
+        [
+            "YY1",
+            "ZNF143",
+            "RAD21",
+            "CTCF",
+            "POLR2A",
+            "SPI1",
+            "EBF1",
+            "PAX5",
+            "RUNX1",
+            "IRF4",
+            "IRF8",
+        ]
+    )
+]
 files["experiment_target"].value_counts()
 
 # %%
 files["filename"] = files["file_download_url"].str.split("/").str[-1]
-files["accession"] = files["file_download_url"].str.split("/").str[-1].str.split(".").str[0]
+files["accession"] = (
+    files["file_download_url"].str.split("/").str[-1].str.split(".").str[0]
+)
 
 # %%
 files["target"].value_counts()
+
+# %%
+files = files.set_index("accession")
 
 # %%
 files.to_csv(bed_folder / "files.csv")
@@ -81,8 +101,8 @@ encode_folder = chd.get_output() / "data" / "encode"
 encode_folder_relative = encode_folder.relative_to(chd.get_git_root())
 
 # %%
-bed_folder = chd.get_output() / "bed" / "gm1282_tf_chipseq"
-bed_folder.mkdir(exist_ok = True, parents = True)
+bed_folder = chd.get_output() / "bed" / "gm1282_structural_chipseq"
+bed_folder.mkdir(exist_ok=True, parents=True)
 
 # %%
 # !ln -s ~/NAS2/wsaelens/projects/chromatinhd/chromatinhd_manuscript/{encode_folder_relative} {encode_folder}
@@ -93,100 +113,17 @@ import urllib
 
 # %%
 opener = urllib.request.build_opener()
-opener.addheaders = [('User-agent', 'Mozilla/5.0')]
+opener.addheaders = [("User-agent", "Mozilla/5.0")]
 urllib.request.install_opener(opener)
 
 # %%
 for _, file in files.iterrows():
-    if not (bed_folder / file["filename"]).exists():
+    if not (encode_folder / file["filename"]).exists():
         print(file["filename"])
-        urllib.request.urlretrieve(file["file_download_url"], bed_folder / file["filename"], )
-
-
-# %%
-files["filename_sorted"] = files["filename"].str.split(".").str[0] + ".sorted.bed"
-for _, file in files.iterrows():
-    if not (bed_folder / file["filename_sorted"]).exists():
-        print(file["filename_sorted"])
-        !bedtools sort -i {bed_folder / file["filename"]} > {bed_folder / file["filename_sorted"]}
-
-# %%
-import pybedtools
-
-# %%
-!wget https://www.encodeproject.org/files/GRCh38_EBV.chrom.sizes/@@download/GRCh38_EBV.chrom.sizes.tsv -O {chd.get_output()}/chromosome.sizes
-chromosome_sizes = chd.get_output() / "chromosome.sizes"
-
-final_files = {}
-for file_accession, file in files.iterrows():
-    final_files[file_accession] = pybedtools.BedTool(bed_folder / file["filename_sorted"]).slop(b = 100, g = chromosome_sizes)
-
-# %%
-import itertools
-
-# %%
-overlaps = []
-combinations = list(itertools.combinations(files.iterrows(), 2))
-for (file_accession_a, file_a), (file_accession_b, file_b) in tqdm.tqdm(combinations):
-    # if not (("SPI" in file_a["experiment_target"]) | ("SPI" in file_b["experiment_target"])):
-    #     continue
-    bed_a = final_files[file_accession_a]
-    bed_b = final_files[file_accession_b]
-    overlap_a = bed_a.intersect(bed_b, u = True).count()
-    overlap_b = bed_b.intersect(bed_a, u = True).count()
-
-    overlaps.append({
-        "file_a": file_accession_a,
-        "file_b": file_accession_b,
-        "overlap_ab": overlap_a,
-        "overlap_ba": overlap_b,
-        "n_a": bed_a.count(),
-        "n_b": bed_b.count(),
-        "jaccard": overlap_a / (bed_a.count() + bed_b.count() - overlap_a)
-    })
-
-# %%
-overlaps = pd.DataFrame(overlaps)
-
-# %%
-overlaps.sort_values("jaccard")
-# %%
-overlaps["target_a"] = files.loc[overlaps["file_a"]]["experiment_target"].str.split("-").str[0].values
-overlaps["target_b"] = files.loc[overlaps["file_b"]]["experiment_target"].str.split("-").str[0].values
-
-# %%
-sns.heatmap(overlaps.set_index(["target_a", "target_b"])["jaccard"].unstack())
-
-# %%
-# %%
-overlaps.sort_values("jaccard")
-
-# %%
-overlaps_mat_ab = overlaps.set_index(["target_a", "target_b"])["overlap_ab"].unstack()
-overlaps_mat_ab = overlaps_mat_ab.reindex(index = overlaps_mat_ab.index, columns = overlaps_mat_ab.index)
-overlaps_mat_ba = overlaps.set_index(["target_a", "target_b"])["overlap_ba"].unstack()
-overlaps_mat_ba = overlaps_mat_ba.reindex(index = overlaps_mat_ab.index, columns = overlaps_mat_ab.index)
-overlaps_mat = overlaps_mat_ab.fillna(0.) + overlaps_mat_ab.T.fillna(0.)
-
-# %%
-sns.heatmap(overlaps_mat)
-
-# %%
-overlaps_mat["TBX21"].hist()
-
-# %%
-overlaps_mat["CEBPB"].mean()
-
-# %%
-overlaps_mat["STAT1"].hist()
-
-# %%
-files["experiment_target"].loc[files["experiment_target"].str.contains("STAT")]
-
-# %%
-import pickle
-pickle.dump(overlaps_mat, open(bed_folder / "overlaps_mat.pickle", "wb"))
-pickle.dump(overlaps, open(bed_folder / "overlaps.pickle", "wb"))
+        urllib.request.urlretrieve(
+            file["file_download_url"],
+            encode_folder / file["filename"],
+        )
 
 # %% [markdown]
 # ## Filter files around genes
@@ -209,25 +146,57 @@ promoters = pd.read_csv(
 )
 
 # %%
-final_files = {}
-for file_accession, file in files.iterrows():
-    final_files[file_accession] = pybedtools.BedTool(bed_folder / file["filename_sorted"])
+import pyBigWig
 
 # %%
-for gene, promoter in tqdm.tqdm(promoters.iterrows(), total = len(promoters), desc = "Genes"):
-    promoter_start = promoter["start"]
-    promoter_end = promoter["end"]
-    promoter_chrom = promoter["chr"]
+import mgzip
+import pickle
 
-    for file_accession, file in tqdm.tqdm(files.iterrows(), total = len(files), desc = gene, leave = False):
-        filtered_file = bed_folder / f"{file_accession}_{gene}.bed"
+for file_accession, file in files.iterrows():
+    print("processing", file_accession)
+    if (
+        bed_folder / f"values_{file_accession}.pickle"
+    ).exists() and file_accession != "ENCFF129YCR":
+        continue
 
-        if not filtered_file.exists():
-            bed = final_files[file_accession]
-            bed = bed.filter(lambda x: x.chrom == promoter_chrom)
-            bed = bed.filter(lambda x: x.start >= promoter_start)
-            bed = bed.filter(lambda x: x.end <= promoter_end)
-            bed = bed.sort()
+    values = {}
+    bw = pyBigWig.open(str(encode_folder / file["filename"]))
+    chroms = bw.chroms()
+    for gene, promoter in tqdm.tqdm(
+        promoters.iterrows(), total=len(promoters), desc="Genes"
+    ):
+        promoter_start = promoter["start"]
+        promoter_end = promoter["end"]
+        promoter_chrom = promoter["chr"]
 
-            bed.saveas(bed_folder / f"{file_accession}_{gene}.bed")
+        minstart = 0
+        maxend = chroms[promoter["chr"]]
+
+        if promoter_start < minstart:
+            promoter_start = minstart
+        if promoter_end > maxend:
+            promoter_end = maxend
+
+        val = bw.values(promoter_chrom, promoter_start, promoter_end, numpy=True)
+        if len(val) < promoter.end - promoter.start:
+            pad_start = promoter_start - promoter.start
+            pad_end = promoter.end - promoter_end
+            val = np.pad(
+                val,
+                (pad_start, pad_end),
+            )
+        if promoter["strand"] == -1:
+            val = val[::-1]
+        values[(file_accession, gene)] = val
+
+    with mgzip.open(
+        str(bed_folder / f"values_{file_accession}.pickle"), "wb"
+    ) as outfile:
+        pickle.dump(values, outfile)
+
+# %%
+
+
+# %%
+# !ls -lh {bed_folder}
 # %%

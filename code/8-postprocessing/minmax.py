8f4fd3c06d0ad96c01a0907160621b44f57692b2
Victor F <victor.fernandezdelima@epfl.ch>
2023-06-29 12:03:57 +0200

    change plotting, minmax functions

--- code/8-postprocessing/minmax.py
+++ code/8-postprocessing/minmax.py
@@ -12,7 +12,7 @@ import matplotlib.patches as patches
 # set folder paths
 folder_root = chd.get_output()
 folder_data = folder_root / "data"
-dataset_name = "hspc"
+dataset_name = "hspc_backup"
 folder_data_preproc = folder_data / dataset_name
 promoter_name, window = "10k10k", np.array([-10000, 10000])
 promoters = pd.read_csv(folder_data_preproc / ("promoters_" + promoter_name + ".csv"), index_col = 0)
@@ -27,16 +27,58 @@ def find_local_minima_maxima(tensor):
 
     for i in range(1, rows - 1):
         for j in range(1, cols - 1):
-            neighbors = tensor[i, j-1:j+2]
+            neighbors = tensor[i-1:i+2, j-1:j+2].flatten()
             center = tensor[i, j]
-            if np.all(center <= neighbors):
+            if np.all(center < neighbors):
                 local_minima.append((i, j))
                 minis.append(tensor[i, j].item())
-            elif np.all(center >= neighbors):
+            elif np.all(center > neighbors):
                 local_maxima.append((i, j))
                 maxis.append(tensor[i, j].item())
 
     return local_minima, local_maxima, minis, maxis
+
+def plot_minmax(tensor, minima, maxima, minis, maxis, gene, plot_dir):
+    minima_y, minima_x = zip(*minima)
+    maxima_y, maxima_x = zip(*maxima)
+
+    minis = np.array(minis)
+    maxis = np.array(maxis)
+    minis = (minis - minis.min()) / (minis.max() - minis.min())
+    maxis = (maxis - maxis.min()) / (maxis.max() - maxis.min())
+
+    plt.figure(figsize=(5, 5))
+    plt.scatter(minima_x, minima_y, c='red', s=1, linewidths=0.5, label='Minima')
+    plt.scatter(maxima_x, maxima_y, c='blue', s=1, linewidths=0.5, label='Maxima')
+
+    plt.title(str(plot_dir).split('/')[-1])
+    plt.xlabel('Positions')
+    plt.ylabel('Latent Time')
+    plt.show()
+
+    file_name = plot_dir / f"{gene}.png"
+    plt.savefig(file_name, dpi=200)
+
+csv_dir = folder_data_preproc / "likelihood_continuous_128_64_32_fold_0"
+plot_dir = folder_data_preproc / "plots/likelihood_continuous_128_64_32_fold_0_>_not_>="
+os.makedirs(plot_dir, exist_ok=True)
+
+#%%
+for x in range(promoters.shape[0]):
+    print(f"{x=}")
+    gene = promoters.index[x]
+    tensor = np.loadtxt(csv_dir / f"{gene}.csv", delimiter=',')
+    minima, maxima, minis, maxis = find_local_minima_maxima(tensor)
+    print(f"{minima=}")
+    print(f"{maxima=}")
+    try:
+        plot_minmax(tensor, minima, maxima, minis, maxis, gene, plot_dir)
+    except ValueError:
+        continue
+
+# %%
+
+
 
 # def plot_minmax(tensor, minima, maxima, gene, plot_dir):
 #     # Create the plot
@@ -67,41 +109,3 @@ def find_local_minima_maxima(tensor):
     
 #     file_name = plot_dir / f"{gene}.png"
 #     plt.savefig(file_name, dpi=200)
-
-def plot_minmax(tensor, minima, maxima, minis, maxis, gene, plot_dir):
-    # Separate the x and y values from the tuples
-    minima_y, minima_x = zip(*minima)
-    maxima_y, maxima_x = zip(*maxima)
-
-    # normalize the minima and maxima values
-    minis = np.array(minis)
-    maxis = np.array(maxis)
-    minis = (minis - minis.min()) / (minis.max() - minis.min())
-    maxis = (maxis - maxis.min()) / (maxis.max() - maxis.min())
-
-    # Plot the minima and maxima as points with varying transparency
-    plt.figure(figsize=(5, 5))
-    plt.scatter(minima_x, minima_y, c='red', alpha=minis, s=1, linewidths=0.5, label='Minima')
-    plt.scatter(maxima_x, maxima_y, c='blue', alpha=maxis, s=1, linewidths=0.5, label='Maxima')
-
-    # Set labels and title
-    plt.title('Local Minima and Maxima')
-    plt.xlabel('Positions')
-    plt.ylabel('Latent Time')
-    plt.show()
-
-    file_name = plot_dir / f"{gene}.png"
-    plt.savefig(file_name, dpi=200)
-
-csv_dir = folder_data_preproc / "evaluate_pseudo_continuous_tensors_128"
-plot_dir = folder_data_preproc / "plots/likelihood_continuous_minmax_128"
-os.makedirs(plot_dir, exist_ok=True)
-
-for x in range(promoters.shape[0]):
-    print(f"{x=}")
-    gene = promoters.index[x]
-    tensor = np.loadtxt(csv_dir / f"{gene}.csv", delimiter=',')
-    minima, maxima, minis, maxis = find_local_minima_maxima(tensor)
-    plot_minmax(tensor, minima, maxima, minis, maxis, gene, plot_dir)
-
-# %%

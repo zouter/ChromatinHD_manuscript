4c56863448c109aead57e33c4cedc91b330b066e
Victor F <victor.fernandezdelima@epfl.ch>
2023-07-10 15:29:17 +0200

    refactor creation of tensor for cutsite plotting

--- code/8-postprocessing/visualizations.py
+++ code/8-postprocessing/visualizations.py
@@ -21,8 +21,8 @@ import matplotlib.pyplot as plt
 # set folder paths
 folder_root = chd.get_output()
 folder_data_preproc = folder_root / "data" / "hspc_backup"
-dataset_name = "myeloid"
 dataset_name = "simulated"
+dataset_name = "myeloid"
 fragment_dir = folder_data_preproc / f"fragments_{dataset_name}"
 df_latent_file = folder_data_preproc / f"MV2_latent_time_{dataset_name}.csv"
 
@@ -59,50 +59,6 @@ mapping = fragments.mapping
 mapping_cutsites = torch.bincount(mapping[:, 1]) * 2
 
 #%%
-dir_csv = folder_data_preproc / "likelihood_continuous_128"
-
-def plot_cutsites_model_continuous(df, gene, n_fragments, directory):
-    fig, (ax1, ax2) = plt.subplots(2, 1, figsize=(15, 20), gridspec_kw={'height_ratios': [1, 1]})
-
-    # Modify subplot configuration
-    plt.subplots_adjust(hspace=0.1, bottom=0.12)
-
-    # Plot cut sites
-    ax1.scatter(df['x'], df['y'], s=1, marker='s', color='black')
-    ax1.set_title(f"{gene} (cut sites = {2 * n_fragments})", fontsize=14)
-    ax1.set_xlabel('Position', fontsize=12)
-    ax1.set_ylabel('Latent Time', fontsize=12)
-    ax1.set_xlim([0, 1])
-    ax1.set_ylim([0, 1])
-    ax1.set_facecolor('white')
-
-    # Plot evaluated probabilities
-    file_name = dir_csv / f"{gene}.csv"
-    probsx = np.loadtxt(file_name, delimiter=',')
-
-    heatmap = ax2.imshow(probsx, cmap='RdBu_r', aspect='auto')
-
-    x_ticks1 = np.linspace(0, 1, num=6)
-    x_tick_labels1 = ['0', '0.2', '0.4', '0.6', '0.8', '1.0']
-    y_ticks1 = np.linspace(0, 1, num=6)
-    y_tick_labels1 = ['0', '0.2', '0.4', '0.6', '0.8', '1.0'][::-1]
-
-    ax2.set_xticks(x_ticks1 * (probsx.shape[1] - 1))
-    ax2.set_xticklabels(x_tick_labels1)
-    ax2.set_yticks(y_ticks1 * (probsx.shape[0] - 1))
-    ax2.set_yticklabels(y_tick_labels1)
-
-    ax2.set_xlabel('Position')
-    ax2.set_ylabel('Latent Time')
-
-    cbar_ax = fig.add_axes([0.15, 0.07, 0.7, 0.02])  # [left, bottom, width, height]
-    cbar = plt.colorbar(heatmap, cax=cbar_ax, orientation='horizontal')
-    cbar.set_label('Probability of finding a cut site at given position and latent time')
-
-    file_name = directory / f"{gene}.png"
-    plt.savefig(file_name, dpi=300)
-
-#%%
 # find dirs of interest
 nbins = (32, )
 nbins = (1024, )
@@ -114,38 +70,39 @@ dirs = sorted([file for file in os.listdir(folder_data_preproc) if pattern in fi
 
 #%%
 for x in range(promoters.shape[0]):
-    print(x)
     gene = promoters.index[x]
     gene_ix = fragments.var.loc[gene]['ix']
+    print(x, gene, gene_ix)
+
     mask = mapping[:,1] == gene_ix
     mapping_sub = mapping[mask]
     coordinates_sub = coordinates[mask]
-    n_fragments = coordinates_sub.shape[0]
 
-    tens = torch.cat((mapping_sub, coordinates_sub), dim=1)
+    tens1 = torch.cat((coordinates_sub[:, 0].unsqueeze(1), mapping_sub[:, 0].unsqueeze(1)), dim=1)
+    tens2 = torch.cat((coordinates_sub[:, 1].unsqueeze(1), mapping_sub[:, 0].unsqueeze(1)), dim=1)
+    tens = torch.cat((tens1, tens2), dim=0)
+
     df = pd.DataFrame(tens.numpy())
-    df.columns = ['cell_ix', 'gene_ix', 'cut_start', 'cut_end']
-    df['height'] = 1
-
+    df.columns = ['x', 'cell_ix']
     df = pd.merge(df, latent_time, left_on='cell_ix', right_index=True)
-    df_long = pd.melt(df, id_vars=['cell_ix', 'gene_ix', 'cell', 'latent_time', 'rank', 'height'], value_vars=['cut_start', 'cut_end'], var_name='cut_type', value_name='position')
-    df_long = df_long.rename(columns={'position': 'x', 'rank': 'y'})
+    df = df.rename(columns={'rank': 'y'})
     
-    # directory = folder_data_preproc / 'plots' / f"cutsites_{dataset_name}"
-    # pf.cutsites(gene, df_long, directory, show=False)
+    directory = folder_data_preproc / 'plots' / f"cutsites_{dataset_name}"
+    pf.cutsites(gene, df, directory, show=False)
+
+    # tens = torch.cat((mapping_sub, coordinates_sub), dim=1)
+    # df = pd.DataFrame(tens.numpy())
+    # df.columns = ['cell_ix', 'gene_ix', 'cut_start', 'cut_end']
+    # df['height'] = 1
+
+    # df = pd.merge(df, latent_time, left_on='cell_ix', right_index=True)
+    # df_long = pd.melt(df, id_vars=['cell_ix', 'gene_ix', 'cell', 'latent_time', 'rank', 'height'], value_vars=['cut_start', 'cut_end'], var_name='cut_type', value_name='position')
+    # df_long = df_long.rename(columns={'position': 'x', 'rank': 'y'})
+    
     # directory = folder_data_preproc / 'plots' / f"cutsites_histo_{dataset_name}"
     # pf.cutsites_histo(gene, df, df_long, n_fragments, directory, show=False)
-
-    directory = folder_data_preproc / f"likelihood_quantile_{dataset_name}"
-    pf.model_quantile(x, latent_torch, fragments, directory, show=False)
-
-    # for dir_x in dirs:
-    #     directory = folder_data_preproc / dir_x
-    #     pf.model_continuous(gene, directory, show=False)
-
-        # plot_cutsites_histo(df, df_long, gene, n_fragments)
-        # plot_cutsites_model_continuous(df_long, gene, n_fragments)
-        # plot_model_quantile(latent_torch, x, fragments)
+    # directory = folder_data_preproc / f"likelihood_quantile_{dataset_name}"
+    # pf.model_quantile(x, latent_torch, fragments, directory, show=False)
 
 #%%
 # calculate the range that contains 90% of the data
@@ -169,6 +126,48 @@ for x in range(promoters.shape[0]):
 # sns.despine()
 
 # fig.savefig(folder_data_preproc / f'plots/n_cutsites.png')
+
+#%%
+def plot_cutsites_model_continuous(df, gene, n_fragments, dir_csv, directory):
+    fig, (ax1, ax2) = plt.subplots(2, 1, figsize=(15, 20), gridspec_kw={'height_ratios': [1, 1]})
+
+    # Modify subplot configuration
+    plt.subplots_adjust(hspace=0.1, bottom=0.12)
+
+    # Plot cut sites
+    ax1.scatter(df['x'], df['y'], s=1, marker='s', color='black')
+    ax1.set_title(f"{gene} (cut sites = {2 * n_fragments})", fontsize=14)
+    ax1.set_xlabel('Position', fontsize=12)
+    ax1.set_ylabel('Latent Time', fontsize=12)
+    ax1.set_xlim([0, 1])
+    ax1.set_ylim([0, 1])
+    ax1.set_facecolor('white')
+
+    # Plot evaluated probabilities
+    file_name = dir_csv / f"{gene}.csv"
+    probsx = np.loadtxt(file_name, delimiter=',')
+
+    heatmap = ax2.imshow(probsx, cmap='RdBu_r', aspect='auto')
+
+    x_ticks1 = np.linspace(0, 1, num=6)
+    y_ticks1 = np.linspace(0, 1, num=6)
+    x_tick_labels1 = ['0', '0.2', '0.4', '0.6', '0.8', '1.0']
+    y_tick_labels1 = ['0', '0.2', '0.4', '0.6', '0.8', '1.0'][::-1]
+
+    ax2.set_xticks(x_ticks1 * (probsx.shape[1] - 1))
+    ax2.set_yticks(y_ticks1 * (probsx.shape[0] - 1))
+    ax2.set_xticklabels(x_tick_labels1)
+    ax2.set_yticklabels(y_tick_labels1)
+
+    ax2.set_xlabel('Position')
+    ax2.set_ylabel('Latent Time')
+
+    cbar_ax = fig.add_axes([0.15, 0.07, 0.7, 0.02])  # [left, bottom, width, height]
+    cbar = plt.colorbar(heatmap, cax=cbar_ax, orientation='horizontal')
+    cbar.set_label('Probability of finding a cut site at given position and latent time')
+
+    file_name = directory / f"{gene}.png"
+    plt.savefig(file_name, dpi=300)
 
 #%%
 # TODO

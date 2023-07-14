bc2adb573782b23e00998ca24e50e75dfc37bf83
Victor F <victor.fernandezdelima@epfl.ch>
2023-07-14 09:25:07 +0200

    update fragment_plot.py move to 8-postprocessing dir update to work with new data clean code, remove redundant lines

--- code/8-postprocessing/fragment_plot.py
+++ code/8-postprocessing/fragment_plot.py
@@ -1,85 +1,62 @@
 # %%
-import scanpy as sc
 import numpy as np
 import pandas as pd
 import chromatinhd as chd
 
-import seaborn as sns
 import matplotlib as mpl
 import matplotlib.pyplot as plt
 from matplotlib.patches import Rectangle
-sns.set_style('ticks')
 
 # %%
 folder_root = chd.get_output()
-folder_data = folder_root / "data"
-dataset_name = "hspc"
-folder_data_preproc = folder_data / dataset_name
+folder_data_preproc = folder_root / "data" / "hspc"
+dataset_name_sub = "MV2"
+dataset_name = 'myeloid'
 
 #%%
 info_genes_cells = pd.read_csv(folder_data_preproc / "info_genes_cells.csv")
-s_genes = info_genes_cells['s_genes'].dropna().tolist()
-g2m_genes = info_genes_cells['g2m_genes'].dropna().tolist()
 hspc_marker_genes = info_genes_cells['hspc_marker_genes'].dropna().tolist()
-lin_myeloid = info_genes_cells['lin_myeloid'].dropna().tolist()
-lin_erythroid = info_genes_cells['lin_erythroid'].dropna().tolist()
-lin_platelet = info_genes_cells['lin_platelet'].dropna().tolist()
+genes = sorted(hspc_marker_genes)
+
+# %%
+lt_myeloid = pd.read_csv(folder_data_preproc / f'{dataset_name_sub}_latent_time_myeloid.csv')
+lt_myeloid.sort_values('latent_time', inplace=True)
+lt_myeloid.index = lt_myeloid["cell"]
+del lt_myeloid["cell"]
+del lt_myeloid["celltype"]
 
 # %%
 promoter_name, window = "10k10k", np.array([-10000, 10000])
+fragments = chd.data.Fragments(folder_data_preproc / f"{dataset_name_sub}_fragments_{dataset_name}" / promoter_name)
+coordinates = fragments.coordinates
+mapping = fragments.mapping
 
 #%%
-lt_myeloid = pd.read_csv(folder_data_preproc / 'MV2_latent_time_myeloid.csv')
-lt_myeloid.index = lt_myeloid["cell"]
-del lt_myeloid["cell"]
-lt_myeloid.sort_values('latent_time', inplace=True)
-
-# %%
-transcriptome = chd.data.Transcriptome(folder_data_preproc / "transcriptome")
-fragments = chd.data.Fragments(folder_data_preproc / "fragments" / promoter_name)
-
-#%%
+transcriptome = chd.data.Transcriptome(folder_data_preproc / f"{dataset_name_sub}_transcriptome")
 transcriptome = transcriptome.adata[lt_myeloid.index, :]
-#%%
 transcriptome.obs.index.name = 'cell'
 transcriptome.obs = transcriptome.obs.join(lt_myeloid, how='left')
 
-genes = sorted(hspc_marker_genes)
-
+# %%
 heatmap_data = transcriptome[:, genes]
 heatmap_data = heatmap_data.X
 
 # %%
 for gene in genes:
-    print('try', gene)
-
     gene_id = transcriptome.var.loc[gene]['Accession']
-    try:
-        gene_ix = fragments.var.loc[gene_id]["ix"]
-        print('success', gene)
-
-    except KeyError:
-        print('failed', gene)
-        continue
-
-    # sc.pl.umap(transcriptome, color = [gene])
-
-    coordinates = fragments.coordinates[fragments.mapping[:, 1] == gene_ix].numpy()
-    mapping = fragments.mapping[fragments.mapping[:, 1] == gene_ix].numpy()
-    outcome = lt_myeloid
-
-    cell_order = outcome.index
-    n_cells = len(cell_order)
+    gene_ix = fragments.var.loc[gene_id]["ix"]
+    print(gene, gene_id, gene_ix)
+    
+    coordinates_sub = coordinates[mapping[:, 1] == gene_ix]
+    mapping_sub = mapping[mapping[:, 1] == gene_ix]
 
     obs = fragments.obs.copy()
-    obs = obs[obs.index.isin(cell_order)]
-    obs.index = obs.index.astype(str)
-    obs = obs.join(outcome, how='left')
-    obs = obs.loc[cell_order]
+    obs = obs.join(lt_myeloid, how='left')
+    obs = obs.loc[lt_myeloid.index]
     obs = obs.set_index("ix")
     obs["y"] = np.arange(obs.shape[0])
 
-    fig, (ax_gex, ax_heatmap, ax_fragments) = plt.subplots(1, 3, figsize = (30, n_cells/300), sharey = True, width_ratios = [0.5, 1, 1.5])
+    fig, (ax_gex, ax_heatmap, ax_fragments) = plt.subplots(1, 3, figsize = (30, len(obs)/300), sharey = True, width_ratios = [0.5, 1, 1.5])
 
     ax_gex.plot(obs["latent_time"], obs["y"])
     ax_gex.set_xlabel('Latent Time', fontsize=20)
@@ -92,8 +69,7 @@ for gene in genes:
     ax_heatmap.set_xticklabels(genes)
     ax_heatmap.tick_params(axis='x', rotation=270)
 
-    highlighted_column = gene
-    highlighted_column_index = genes.index(highlighted_column)
+    highlighted_column_index = genes.index(gene)
     rect = Rectangle((highlighted_column_index, 0), 1, heatmap_data.shape[0], edgecolor='black', facecolor='none', linewidth=3)
     ax_heatmap.add_patch(rect)
 
@@ -101,12 +77,11 @@ for gene in genes:
     highlighted_label.set_weight('bold')
     highlighted_label.set_backgroundcolor('lightgray')
 
-    # cbar = ax_heatmap.figure.colorbar(im, ax=ax_heatmap)
-
     ax_fragments.set_xlim(*window)
-    ax_fragments.set_ylim(0, n_cells)
+    ax_fragments.set_ylim(0, len(obs))
 
-    for (start, end, cell_ix) in zip(coordinates[:, 0], coordinates[:, 1], mapping[:, 0]):
+    for (start, end, cell_ix) in zip(coordinates_sub[:, 0], coordinates_sub[:, 1], mapping_sub[:, 0]):
+        cell_ix = int(cell_ix)
         if cell_ix in obs.index:
             rectangles = [
                 mpl.patches.Rectangle((start, obs.loc[cell_ix, "y"]), end - start, 10, fc = "#33333333", ec = None, linewidth = 0),
@@ -132,6 +107,7 @@ for gene in genes:
             highlighted_label.set_weight('bold')
             highlighted_label.set_backgroundcolor('lightgray')
 
-    fig.savefig(folder_data_preproc / f"plots/fragments_{gene}.png", transparent=False, dpi = 300)
+    fig.show()
+    # fig.savefig(folder_data_preproc / f"plots/fragments_{gene}.png", transparent=False, dpi = 300)
 
 # %%
